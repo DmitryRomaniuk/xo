21b904a7834406df830ba587f7753f9eeafa4dd6
Dzmitry Ramaniuk <kanumowa@gmail.com>
2016-11-07 21:45:12 +0300

    reworked Components

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,90 +3,94 @@
  */
 'use strict';
 var data_1 = { counter_y1:0,counter_y2:0,counter_y3:0,counterx1:0,counterx2:0,counterx3:0 };
-var counter_y1,counter_y2,counter_y3,counterx1,counterx2,counterx3;
 Vue.component('my-game', {
   template: `
-                <div class="game">
-                <my-row class="row-0" v-on:click="counter_y1 += 1"></my-row>
-                <my-row class="row-1" v-on:click="counter_y2 += 1"></my-row>
-                <my-row class="row-2" v-on:click="counter_y3 += 1"></my-row>
-                </div>
+        <div class="game">
+            <my-row class="row-0" row=0 v-on:click="newClick"></my-row>
+            <my-row class="row-1" row=1 v-on:click=""></my-row>
+            <my-row class="row-2" row=2 v-on:click=""></my-row>
+        </div>
             `,
-        data: function () {
-            //console.log(data);
-            return data_1
-        }
+            methods: {
+                newClick: function () {
+                    console.log('click')
+                    this.$emit('asdf','asdfsa')
+                }
+            }
 })
 Vue.component('my-row', {
   template: `
-            <div class="row-box">
-                <my-col class="colu-0" v-on:click="cx1()"></my-col>
-                <my-col class="colu-1" v-on:click="counterx2 += 1"></my-col>
-                <my-col class="colu-2" v-on:click="counterx3 += 1"></my-col>
-            </div>
+        <div class="row-box" >
+            <my-col class="colu-0" :row="row" col=0 v-on:click="" @position-click="handleClick"></my-col>
+            <my-col class="colu-1" :row="row" col=1 v-on:click=""></my-col>
+            <my-col class="colu-2" :row="row" col=2 v-on:click=""></my-col>
+        </div>
             `,
+  props: ['row'],
     methods: {
-        cx1: function(event) {
-            console.log('mycx1');
-        }
-    },
-    data: function () {
-        //console.log(data);
-        return data_1
+    handleClick: function(positX){
+        //console.log(positX);
+        /*fetch('/', {
+            method: 'post',
+            body: JSON.stringify(posit)
+        });   */
     }
+}
 })
 Vue.component('my-col', {
-  template: `<div class="cell">
-                    <div class="inner-fillet" v-on:click="counterx3 += 1">{{data_1}}
-                        <i class="fa fa-check" aria-hidden="true"></i>
+  template: `<div class="cell" v-on:click="cx2(event)">
+                    <div class="inner-fillet">
+                        <i class="fa fa-check" aria-hidden="true"
+                        v-bind:class="[isChecked ? 'busy' : 'free']"></i>
                     </div>
             </div>
             `,
-        data: function () {
-            //console.log(data);
-            return data_1
+    props: ['row','col'],
+    data: function () {
+        //console.log('ROW'+this.row)
+        //console.log('COL'+this.col)
+        var posX=this.col;
+        var posY=this.row;
+        var isChecked = arr[posX][posY];
+        return {arr: arr, isChecked:isChecked, posX:posX, posY:posY}
+    },
+    computed: {
+    classObject: function () {
+        this.isChecked = arr[this.posX][this.posY];
+        return {
+        free: !this.isChecked,
+        busy: this.isChecked
         }
+    }
+    },
+    methods: {
+        cx2: function(event) {
+            arr[this.posY][this.posX] = true;
+            //console.log(arr[0][0],arr[0][1],arr[0][2],arr[1][0],arr[1][1],arr[1][2],arr[2][0],arr[2][1],arr[2][2]);
+            console.log(this.posX+ ' '+this.posY+ ' '+this.isChecked)
+            this.posClick = {posX:this.posX,posY:this.posY};
+            //console.log('emit-data');
+            this.$emit('position-click', 'data')
+            return arr
+        }
+    }
 })
-var arrX = [false,false,false];
-var arr = [arrX,arrX,arrX];
-var aX,aY;
-//var arr = [[0,1,2],[3,4,5],[6,7,8]];
+var arrX = new Array(3);
+var arr = [Object.create(arrX),Object.create(arrX),Object.create(arrX)];
 new Vue({
     el: '#gameClick',
     methods: {
         onClick: function(event) {
-            var posX,posY;
-        if (event.target.className.indexOf("colu-")>=0) {
-            posX=event.target.className
-            posY=event.target.parentNode.className
-        }
-        if (event.target.parentNode.className.indexOf("colu-")>=0) {
-            posX=event.target.parentNode.className
-            posY=event.target.parentNode.parentNode.className
-        }
-        if (event.target.parentNode.parentNode.className.indexOf("colu-")>=0) {
-            posX=event.target.parentNode.parentNode.className
-            posY=event.target.parentNode.parentNode.parentNode.className
-        }
-        posX=posX.match( /colu-\d/ig )[0];
-        posY=posY.match( /row-\d/ig )[0];
-        posX=posX[posX.length-1];
-        posY=posY[posY.length-1];
-        arr[posY][posX] = true;
-        console.log({
-            counterW:0,
-            counter_y1: 0,
-            counter_y2: 0,
-            counter_y3: 0,
-            counterx1: 0,
-            counterx2: 0,
-            counterx3: 0,
-        });
-        //console.log(JSON.stringify({posX:posX,posY:posY}));
+    },
+    handleClick: function(positX){
+        //console.log(positX);
         /*fetch('/', {
             method: 'post',
-            body: JSON.stringify({posX:posX,posY:posY})
-        });*/
+            body: JSON.stringify(posit)
+        });   */
+    },
+    randomClick: function(message) {
+        console.log(message)
     }
 }
 })
