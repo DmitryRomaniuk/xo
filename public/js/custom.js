668b7bd574f1b39eda3516f25e8e38018d32d53d
Dzmitry_Ramaniuk2 <Dzmitry_Ramaniuk2@epam.com>
2016-11-15 18:10:15 +0300

    added this.$root.$on

--- public/js/custom.js
+++ public/js/custom.js
@@ -5,15 +5,11 @@
 Vue.component('my-game', {
     template: `
             <div class="game">
-                <my-row class="row-0" row=0 v-on:click="" :arr="this.arr" @position-click="handleClick"></my-row>
-                <my-row class="row-1" row=1 v-on:click="" :arr="this.arr" @position-click="handleClick"></my-row>
-                <my-row class="row-2" row=2 v-on:click="" :arr="this.arr" @position-click="handleClick"></my-row>
+                <my-row class="row-0" row=0 v-on:click="" @position-click="handleClick"></my-row>
+                <my-row class="row-1" row=1 v-on:click="" @position-click="handleClick"></my-row>
+                <my-row class="row-2" row=2 v-on:click="" @position-click="handleClick"></my-row>
             </div>
                 `,
-    props: ['arr'],
-    data: function() {
-        console.log(this.arr);
-    },
     methods: {
         handleClick: function(position){
             this.$emit('position-click', position)
@@ -23,12 +19,12 @@ Vue.component('my-game', {
 Vue.component('my-row', {
     template: `
             <div class="row-box">
-                <my-col class="colu-0" :row="row" :arr="0" col=0 v-on:click="" @position-click="handleClick"></my-col>
-                <my-col class="colu-1" :row="row" :arr="0" col=1 v-on:click="" @position-click="handleClick"></my-col>
-                <my-col class="colu-2" :row="row" :arr="0" col=2 v-on:click="" @position-click="handleClick"></my-col>
+                <my-col class="colu-0" :row="row" col=0 v-on:click="" @position-click="handleClick"></my-col>
+                <my-col class="colu-1" :row="row" col=1 v-on:click="" @position-click="handleClick"></my-col>
+                <my-col class="colu-2" :row="row" col=2 v-on:click="" @position-click="handleClick"></my-col>
             </div>
                 `,
-    props: ['row','arr'],
+    props: ['row'],
     methods: {
     handleClick: function(position){
         this.$emit('position-click', position)
@@ -44,12 +40,10 @@ Vue.component('my-col', {
                     </div>
             </div>
             `,
-    props: ['row','col','arr'],
+    props: ['row','col'],
     data: function () {
         var posX=this.col;
         var posY=this.row;
-        var arrX = new Array(3);
-        var arr = [Object.create(arrX),Object.create(arrX),Object.create(arrX)];
         var playerOne = (arr[posY][posX] === "playerOne")? true: false;
         var playerTwo = (arr[posY][posX] === "playerTwo")? true: false;
         var isChecked = (arr[posY][posX])? true: false;
@@ -57,38 +51,66 @@ Vue.component('my-col', {
         return {arr, isChecked, posX, posY, playerOne, playerTwo, posClick}
     },
     computed: {
-    classObject: function () {
-        return {
-        free: !this.isChecked,
-        busy: this.isChecked
+        classObject: function () {
+            return {
+            free: !this.isChecked,
+            busy: this.isChecked
+            }
         }
-    }
+    },
+    mounted: function () {
+        this.$root.$on('position-click', function (posClick) {
+            this.arr[1][1] = "playerTwo";
+            console.log(posClick.posX + " " + posClick.posY)
+            if (this.posX === posClick.posX) {
+                if (this.posY === posClick.posY) {
+                    this.playerOne = (arr[this.posY][this.posX] === "playerOne")? true: false;
+                    this.playerTwo = (arr[this.posY][this.posX] === "playerTwo")? true: false;
+                    this.isChecked = (arr[this.posY][this.posX])? true: false;
+                }
+            }
+        })
     },
     methods: {
         cx2: function(event) {
-            arr[this.posY][this.posX] = "playerOne";
-            this.isChecked = arr[this.posY][this.posX];
-            // this.playerOne = !this.playerOne;
-            // this.playerTwo = !this.playerTwo;
-            //console.log(this.posX+ ' '+this.posY+ ' '+this.isChecked+ ' '+arr[this.posY][this.posX])
-            this.posClick = {posX:this.posX,posY:this.posY};
-            // console.log('emit-data');
-            this.$emit('position-click', this.posClick)
+            this.arr[this.posY][this.posX] = "playerOne";
+            console.log(arr);
+            this.isChecked = (this.arr[this.posY][this.posX])? true: false;
+            this.playerOne = (this.arr[this.posY][this.posX] === "playerOne")? true: false;
+            this.playerTwo = (this.arr[this.posY][this.posX] === "playerTwo")? true: false;
+            this.posClick = {posX:this.posX,posY:this.posY, arr: this.arr};
+            this.$root.$emit('position-click', this.posClick)
             return arr
         }
     }
 })
+var arrX = new Array(3);
+var arr = [Object.create(arrX),Object.create(arrX),Object.create(arrX)];
 // var arr = [["playerOne","playerOne","playerTwo"],["playerOne","playerTwo","playerOne"],["playerTwo","playerOne","playerOne"]];
 new Vue({
     el: '#gameClick',
-    data: function() {
-        var arrX = new Array(3);
-        var arr = [Object.create(arrX),Object.create(arrX),Object.create(arrX)];
-        console.log(this.arr);
-    },
     methods: {
         onClick: function(event) {
     },
+    data: function() {
+        return {arr}
+    },
+    // beforeUpdate: function () {
+    //     this.$root.$on('position-click', function (posClick) {
+    //         console.log('component parent')
+    //         console.log(posClick.posX + " " + posClick.posY)
+    //         arr[1][1] === "playerTwo";
+    //         this.posClick = {posX:1,posY:1, arr: this.arr};
+    //         this.$root.$emit('component-click', this.posClick)
+    //         // if (this.posX === posClick.posX) {
+    //         //     if (this.posY === posClick.posY) {
+    //         //         this.playerOne = (arr[this.posY][this.posX] === "playerOne")? true: false;
+    //         //         this.playerTwo = (arr[this.posY][this.posX] === "playerTwo")? true: false;
+    //         //         this.isChecked = (arr[this.posY][this.posX])? true: false;
+    //         //     }
+    //         // }
+    //     })
+    // },
     handleClick: function(positX){
         console.log(positX.posX);
         console.log(positX.posY);
@@ -102,7 +124,7 @@ new Vue({
     },
     template: `
         <div class="wrap-game" id="gameClick">
-            <div is="my-game" @position-click="handleClick" :arr="arr">
+            <div is="my-game" @position-click="handleClick">
             </div>
         </div>
     `
